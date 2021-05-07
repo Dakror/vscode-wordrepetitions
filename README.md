04d43f49a11c26616b72ad979a26a056999efd2b
Dakror <mail@dakror.de>
2021-05-07 16:44:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# VSCode Extension - Word Repetitions
+
+This extension highlights word repetitions based on two configuration values it offers.
+
+You can select the minimum distance for word repetitions and words to exclude, like common filling words.
---
+++ src/extension.ts
@@ -0,0 +1,58 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    let diag = vscode.languages.createDiagnosticCollection("wordrepeats");
+
+    vscode.commands.registerTextEditorCommand("wordrepeats.clearRepetitions", async () => {
+        diag.clear();
+    });
+
+    vscode.commands.registerTextEditorCommand("wordrepeats.findRepetitions", async () => {
+        const editor = vscode.window.activeTextEditor;
+        const config = vscode.workspace.getConfiguration("wordrepeats");
+        if (editor) {
+            const minDistance: number = config.get("minDistance")!;
+            const excluded: string = config.get("excludedWords")!;
+            const excludedList = excluded.split(",").map((x) => x.toLowerCase());
+            const words = editor.document.getText().split(/\s/);
+            let map = new Map<string, number>();
+            let ctr = 0;
+            let pos = 0;
+            let regex = new RegExp(/^[a-zA-ZöäüÖÄÜß]+$/);
+            let diags: vscode.Diagnostic[] = [];
+            for (let word of words) {
+                if (word.match(regex) && !excludedList.includes(word)) {
+                    let val = map.get(word);
+                    if (val !== undefined && ctr - val < minDistance) {
+                        // add squiggle
+                        diags.push(
+                            new vscode.Diagnostic(
+                                new vscode.Range(
+                                    editor.document.positionAt(pos),
+                                    editor.document.positionAt(pos + word.length)
+                                ),
+                                `Duplicate word within min distance (${ctr - val} words ago)`,
+                                vscode.DiagnosticSeverity.Information
+                            )
+                        );
+                    }
+                    val = ctr;
+                    map.set(word, val);
+
+                    ctr++;
+                }
+
+                pos += word.length + 1;
+            }
+
+            diag.set(editor.document.uri, diags);
+        }
+    });
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
